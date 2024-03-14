658df193973c4205ff0d0d87be069e1329da3e10
Gurudatt S A <gurudatt.gd04@gmail.com>
2024-03-14 22:39:27 +0530

    Fixing flaky test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -14,6 +14,8 @@ describe('Sign Up', () => {
     cy.get('select[name="department"]').select('core')
     // select the "git-it" course
     cy.get('select[name="course"]').select('git-it')
+    //Ensure drop has seleted value before submission
+    cy.get('select[name="course"]').invoke('val').should('eql', 'git-it')
     // submit the form
     cy.get('input[type="submit"]').click()
     // the "Saved!" message should appear
